43bb329a8c8e542249045a009cfa66b4691a8632
maivankien <maivankien1710@gmail.com>
2024-03-14 21:05:25 +0700

    delete authen and update query

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -7,15 +7,23 @@ module.exports = {
             SELECT id, \`date\`, energy_consumed as value
             FROM energy_usage_daily
             WHERE date >= ? and date <= ?`
-            
+
         return (await pool.query(sql, [dateStart, dateEnd]))[0]
     },
     async getEnergyUsageByYear(year) {
-        const sql = 
-            `SELECT id, \`month\`, energy_consumed as value
-            FROM energy_usage_monthly
-            WHERE \`year\` = ?
-            ORDER BY id ASC`
+        // const sql =
+        //     `SELECT id, \`month\`, energy_consumed as value
+        //     FROM energy_usage_monthly
+        //     WHERE \`year\` = ?
+        //     ORDER BY id ASC`
+        const sql = `
+            SELECT YEAR(date) AS year,
+                MONTH(date) AS month,
+                SUM(energy_consumed) AS value
+            FROM energy_usage_daily
+            WHERE YEAR(date) = ?
+            GROUP BY YEAR(date), MONTH(date)
+            ORDER BY YEAR(date), MONTH(date)`
 
         return (await pool.query(sql, [year]))[0]
     }
