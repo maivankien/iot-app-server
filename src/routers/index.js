0db8a3da9f0f41f747c38fc371e293a576849be6
maivankien <maivankien1710@gmail.com>
2024-03-21 21:52:30 +0700

    update middleware authen

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,13 +6,10 @@ const userMiddleware = require('../middlewares/user.middleware')
 
 
 const route = (app) => {
-  app.use('/api/user', user)
-  // app.use('/api/device', userMiddleware.authUser, device)
-  // app.use('/api/report', userMiddleware.authUser, report)
-  // app.use('/api/schedule', userMiddleware.authUser, schedule)
-  app.use('/api/report', report)
-  app.use('/api/device', device)
-  app.use('/api/schedule', schedule)
+    app.use('/api/user', user)
+    app.use('/api/device', userMiddleware.authUser, device)
+    app.use('/api/report', userMiddleware.authUser, report)
+    app.use('/api/schedule', userMiddleware.authUser, schedule)
 }
 
 module.exports = route
